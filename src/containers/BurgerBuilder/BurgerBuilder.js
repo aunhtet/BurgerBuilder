f334a911caf92550aa95715956da26a5f5712d8a
Nay Lin Aung <aunhtet@gmail.com>
2020-01-13 19:25:03 +0630

    Comit on 13-1-2020

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,28 +83,16 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true});
-        const order ={
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name : 'Nay Lin',
-                address : {
-                    street: 'test street',
-                    zipcode: '41323', 
-                    country: 'Myanmar'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = [];
+        for (let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json',order)
-            .then((response)=> {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch((error) => {
-                this.setState({loading: false, purchasing: false});
-            });
+        queryParams.push('price='+ this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: "/checkout",
+            search: '?' + queryString
+        });
     }
 
     render(){
